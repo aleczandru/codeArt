82501dd5cba81e0b9038d9200c90f6632ead4d54
aleczandru <alexandru.nistor.89@gmail.com>
2013-12-26 20:56:04 +0200

    Update Profile

--- Frontend/CodeArt.Frontend/Scripts/main.js
+++ Frontend/CodeArt.Frontend/Scripts/main.js
@@ -1,3 +1,16 @@
 ﻿$(function() {
+    var technologyButton = $("#technology-button");
+    var technologyInput = $("#technology-input");
+    var technologyMultilineSelect = $("#technology-multiline-select");
+    
+    technologyButton.on("click", function () {
+        var technology = technologyInput.val();
+        var option = "<option>" + technology + "</option>";
+        if (technology.length > 0) {
+            technologyMultilineSelect.append(option);
+            technologyInput.val("");
+        }
 
+        return false;
+    });
 })
